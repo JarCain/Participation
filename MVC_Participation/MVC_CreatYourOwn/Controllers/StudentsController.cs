b0476404d15d131c45b2b63c6792a1727c172201
Dakota Cain <cain_dakota@ou.edu>
2020-04-13 15:54:34 -0500

    Attempt at understanding exam

---
+++ MVC_Participation/MVC_CreatYourOwn/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_CreatYourOwn.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Students()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ MVC_Participation/MVC_CreatYourOwn/Models/StudentInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CreatYourOwn.Models
+{
+    public class StudentInfo
+    {
+        public string Name { get; set; }
+
+        public StudentInfo()
+        {
+            Name = "";
+        }
+        
+    }
+}
